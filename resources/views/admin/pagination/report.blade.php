49dd4275ed0113ea87c79faa95e1da22b5e5d277
VietCuongHO <106673386+VietCuongHO@users.noreply.github.com>
2023-05-08 22:48:48 +0700

    edit report

---
+++ resources/views/admin/pagination/report.blade.php
@@ -0,0 +1,46 @@
+<div class="board">
+    <table width="100%" class="table table-hover" style="margin-bottom: 0px">
+        <thead>
+            <tr>
+                <td>Date</td>
+                <td>Time</td>
+                <td>Name</td>
+                <td>ID</td>
+                <td>Office</td>
+                <td>Department</td>
+                <td>Comment</td>
+                <td>Status</td>
+            </tr>
+        </thead>
+        <tbody>
+        @foreach ($list as $item )
+            @php $dt = new DateTime($item->created_at); @endphp
+            <tr>
+                <td>
+                    <p class="fw-bold">{{ $dt->format('d/m/Y') }}</p>
+                </td>
+                <td>
+                    <p>{{ $dt->format('H:i:s') }}</p>
+                </td>
+                <td>
+                    <p>{{ $item->last_name . ' ' . $item->first_name }}</p>
+                </td>
+                <td>
+                    <p>{{ $item->id }}</p>
+                </td>
+                <td class="office">
+                    <p>{{ $item->office_name }}</p>
+                </td>
+                <td class="department">
+                    <p>{{ $item->department }}</p>
+                </td>
+                <td>
+                    <p class="fw-bold">{{ $item->comment }}</p>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+
+@include('admin.components.pagination')
