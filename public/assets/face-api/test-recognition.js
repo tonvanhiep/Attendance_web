6c4accee1afb01056d5f4defbee2790ce3d6be2f
tonvanhiep <tonvanhiepdragon@gmail.com>
2023-05-18 10:58:21 +0700

    add file test recognition

--- public/assets/face-api/test-recognition.js
+++ public/assets/face-api/test-recognition.js
@@ -66,7 +66,7 @@ async function startTest(faceMatcher) {
     var _true = 0;
     var _false = 0;
     for (let i = 0; i < arrLabel.length; i++) {
-        for (let j = 0; j < arrLabel[i].sl; j++) {
+        for (let j = 1; j < arrLabel[i].sl; j++) {
             var arr = {
                 'stt' : stt++,
                 'image' : urlImage + folderName + j + '.jpeg',
